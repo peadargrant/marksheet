7deb5b2281c6352aceccbcc05f065bce1fb9665f
Peadar Grant <peadargrant@gmail.com>
2015-08-26 09:30:04 +0100

    allows interactive console usage

--- src/main/java/com/peadargrant/marksheet/tool/MarksheetTool.java
+++ src/main/java/com/peadargrant/marksheet/tool/MarksheetTool.java
@@ -6,28 +6,40 @@ package com.peadargrant.marksheet.tool;
 
 import com.peadargrant.marksheet.core.MarkSheetGenerator;
 import java.io.File;
+import java.util.Scanner;
 
 /**
  *
  * @author Peadar Grant
  */
 public class MarksheetTool {
-    
+
     public static void main(String[] args) throws Exception {
-        
+
         System.out.println("marksheet generator");
-        
-        if ( args.length != 2 ) {
+
+        if (args.length != 2 && !(args.length == 1 && args[0].equals("--"))) {
             System.err.println("incorrect number of arguments received!");
-            System.err.println("usage: marksheet <input filename> <output filename>");
+            System.err.println("batch usage: marksheet <input filename> <output filename>");
+            System.err.println("interactive usage: marksheet --");
             System.exit(1);
         }
-        
-        File inputFile = new File(args[0]);
-        File outputFile = new File(args[1]);
-        
+
+        File inputFile;
+        File outputFile;
+        if ("--".equals(args[0])) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("enter input filename: ");
+            inputFile = new File(scanner.next());
+            System.out.print("enter output filename: ");
+            outputFile = new File(scanner.next());
+        } else {
+            inputFile = new File(args[0]);
+            outputFile = new File(args[1]);
+        }
+
         MarkSheetGenerator mg = new MarkSheetGenerator(inputFile, outputFile);
         mg.generateMarkSheet();
     }
-    
+
 }
